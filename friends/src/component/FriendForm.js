b05e74e939dafb527595c9bd7667c4bfa9f4b05b
teej387 <teej387@yahoo.com>
2019-06-25 22:27:39 -0500

    Changed FriendForm to a class component.

--- friends/src/component/FriendForm.js
+++ friends/src/component/FriendForm.js
@@ -1,11 +1,25 @@
 import React from 'react'
 
-function FriendForm(props) {
-  return (
-    <div className='friendForm'>
+class FriendForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      postForm: {}
+    }
+  }
 
-    </div>
-  )
+  render() {
+    return (
+      <div className='friendForm'>
+        <form className='addFriend' action='http://localhost:5000/friends' method='post'>
+          <input type='text' name='name' placeholder='Name' /><br />
+          <input type='number' name='age' placeholder='Age' /><br />
+          <input type='email' name='email' placeholder='Email' /><br />
+          <input type='submit' value='Add' />
+        </form>
+      </div>
+    )
+  }
 }
 
 export default FriendForm;
--- friends/src/component/FriendList.js
+++ friends/src/component/FriendList.js
@@ -1,13 +1,15 @@
 import React from 'react'
 import Friend from './Friend'
+import FriendForm from './FriendForm'
 
 function FriendList(props) {
-  console.log(props.friends)
   return (
     <div className='friendList'>
       {props.friends.map(friend => {
         return <Friend friends={friend} key={friend.id} />
       })}
+      <FriendForm />
+      {console.log(props.friends)}
     </div>
   )
 }
